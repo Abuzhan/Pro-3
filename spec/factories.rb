60c0bb60aaff1fc857f04a69967b094b8c942523
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2016-07-30 07:02:53 +0600

    Finish user signup part one

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name      "Abuzhan Aidaraliyev"
+		email     "abuzhan.aidaraliyev@nu.edu.kz"
+		password  "vpcf13s8r"
+		password_confirmation "vpcf13s8r"
+	end
+end
