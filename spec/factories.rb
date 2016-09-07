ceb49d1e15e848fcb0170f30c5ee20e9b0f7e6bf
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2016-09-07 17:56:11 +0600

    Bye Hartl

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,9 @@ FactoryGirl.define do
 		sequence(:email) { |n| "person_#{n}@abuzhan.org" }
 		password  "foobar"
 		password_confirmation "foobar"
+
+		factory :admin do
+			admin true
+		end
 	end
 end
