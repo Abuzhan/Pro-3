69747842811ab6ec26689d4800e94e86ed54a2a2
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2016-10-10 16:16:11 +0600

    New DB

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919180521) do
+ActiveRecord::Schema.define(version: 20161010101223) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -21,9 +21,9 @@ ActiveRecord::Schema.define(version: 20160919180521) do
     t.string   "password_digest"
     t.string   "remember_token"
     t.boolean  "admin",           default: false
-    t.integer  "phone_number"
     t.string   "car_type"
     t.string   "city"
+    t.integer  "phone_number"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
