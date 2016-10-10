bda3af3ff9b6849c2bead324ad80ed641c0b5114
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2016-10-10 15:49:45 +0600

    Hope

--- db/migrate/20160919170649_add_phone_number_to_users.rb
+++ db/migrate/20160919170649_add_phone_number_to_users.rb
@@ -1,5 +1,5 @@
 class AddPhoneNumberToUsers < ActiveRecord::Migration
   def change
-    change_column :users, :phone_number, :integer, :limit => 8
+    add_column :users, :phone_number, :bigint
   end
 end
