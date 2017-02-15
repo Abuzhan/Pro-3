eba8bfec88847bbd24dcb8d41e274679a25b8c06
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2017-02-15 18:05:15 +0600

    Heroku postresql

--- db/migrate/20170123124435_change_column_type_car_type_id_tointegerin_users.rb
+++ db/migrate/20170123124435_change_column_type_car_type_id_tointegerin_users.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypeCarTypeIdTointegerinUsers < ActiveRecord::Migration
   def self.up
-  	change_column :users, :car_type_id, :integer
+  	change_column :users, :car_type_id, 'integer USING CAST("column_to_change" AS integer)'
   end
 
   def self.down
