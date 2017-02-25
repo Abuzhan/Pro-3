abd34701f5a132a6fb966edea8327311b6f9a1b4
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2017-02-26 03:34:01 +0600

    migration for Heroku

--- db/migrate/20170123124816_change_column_type_city_id_to_integer_in_users.rb
+++ db/migrate/20170123124816_change_column_type_city_id_to_integer_in_users.rb
@@ -1,6 +1,6 @@
 class ChangeColumnTypeCityIdToIntegerInUsers < ActiveRecord::Migration
   def self.up
-  	change_column :users, :city_id, :integer #'integer USING CAST("city_id" AS integer)'
+  	change_column :users, :city_id, 'integer USING CAST("city_id" AS integer)'
   end
 
   def self.down
