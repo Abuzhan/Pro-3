99593fc856b557c34d47a4136a50df018d2507bd
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2017-02-27 16:46:43 +0600

    validation fixes for schedules

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -5,7 +5,9 @@ class Schedule < ActiveRecord::Base
 	VALID_DAY_REGEX = /(1|2|3|4|5|6|7)/i
 	
 	validates :day, presence: true, format: { with: VALID_DAY_REGEX }, length: { is: 1 }
-	validates :opening_time, presence: true
-	validates :closing_time, presence: true
+	validates :opening_minute, presence: true
+	validates :closing_minute, presence: true
+	validates :opening_hour, presence: true
+	validates :closing_hour, presence: true
 	
 end
