fe4c344721131859dc61a12bfab72c061ad400ce
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2017-03-01 16:23:39 +0600

    favorites api fixes

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -4,12 +4,12 @@ class Api::V1::FavoritesController < Api::V1::BaseController
 	before_filter :authenticate_user!, only: [:show, :index]
 
 	def index
-		@user = User.find_by(params[:id])
-		@favorites = @user.favorite_carwashes
+		user = User.find_by(params[:id])
+		@favorites = user.favorite_carwashes
 		#render :json => {
 		#	:favorites => @favorites.as_json(only: [:id, :name])}
 		render(
-			json: Api::V1::FavoriteSerializer.new(@user).to_json,
+			json: Api::V1::FavoriteSerializer.new(user).to_json,
 			status: 200
 		)
 	end
