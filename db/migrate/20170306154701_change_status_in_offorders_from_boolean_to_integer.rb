3c2b3e513fe6df5397f6a11adf44f1c2b1be058b
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2017-03-06 21:58:49 +0600

    new DB 5

--- db/migrate/20170306154701_change_status_in_offorders_from_boolean_to_integer.rb
+++ db/migrate/20170306154701_change_status_in_offorders_from_boolean_to_integer.rb
@@ -1,6 +1,6 @@
 class ChangeStatusInOffordersFromBooleanToInteger < ActiveRecord::Migration
 	def self.up
-  		change_column :offorders, :status, :integer
+  		change_column :offorders, :status, 'integer USING CAST("status" AS integer)'
   	end
 
  	def self.down
