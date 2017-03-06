32d3d346bc158ec2803847835f783c83cd80846b
Abuzhan <abuzhan.aidaraliyev@nu.edu.kz>
2017-03-06 21:56:29 +0600

    new DB 4

--- db/migrate/20170306154550_change_default_value_for_status_in_orders.rb
+++ db/migrate/20170306154550_change_default_value_for_status_in_orders.rb
@@ -1,5 +1,5 @@
 class ChangeDefaultValueForStatusInOrders < ActiveRecord::Migration
   def change
-  	change_column_default :orders, :status, nilrake
+  	change_column_default :orders, :status, nil
   end
 end
